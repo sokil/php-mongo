fd0948113c969139d9ebcad1fb23c16303c547e7
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-09-07 15:07:02 +0300

    coverage

--- tests/GridFsTest.php
+++ tests/GridFsTest.php
@@ -44,15 +44,6 @@ class GridFsTest extends \PHPUnit_Framework_TestCase
         $fs->delete();
     }
 
-    public function testInitFileWithArray()
-    {
-        $file = new GridFSFile(self::$database->getGridFS('images'), array(
-            'param' => 'value'
-        ));
-
-        $this->assertInstanceOf('\Sokil\Mongo\GridFSFile', $file);
-    }
-
     public function testCreateFromBinary()
     {
         $fs = self::$database->getGridFs('images');
@@ -80,33 +71,6 @@ class GridFsTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\Sokil\Mongo\GridFSFile', $file);
         
         $this->assertEquals(1, $file->get('meta1'));
-    }
-    
-    public function testSave()
-    {
-        $fs = self::$database->getGridFs('images');
-        
-        $id = $fs->storeBytes('somebinarydata', array(
-            'meta1' => 1,
-            'meta2' => 2,
-        ));
-        
-        $file = $fs->getFileById($id);
-        $file->set('meta1', 777)->save();
-        
-        $file = $fs->getFileById($id);
-        $this->assertEquals(777, $file->get('meta1'));
-    }
-    
-    public function testGetFilename()
-    {
-        $fs = self::$database->getGridFs('images');
-        
-        $id = $fs->storeBytes('somebinarydata', array(
-            'filename' => '/etc/hosts',
-        ));
-        
-        $this->assertEquals('/etc/hosts', $fs->getFileById($id)->getFilename());        
     }
     
     public function testDelete()
