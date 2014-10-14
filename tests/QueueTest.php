99ade45610b17930e35e62dd8bb7865192b230b7
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-10-14 18:46:14 +0300

    isolate tests

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -8,69 +8,73 @@ class QueueTest extends \PHPUnit_Framework_TestCase
      *
      * @var \Sokil\Mongo\Database
      */
-    private static $database;
+    private $database;
     
-    public static function setUpBeforeClass()
+    /**
+     *
+     * @var \Sokil\Mongo\Queue
+     */
+    private $queue;
+    
+    public function setUp()
     {
         // connect to mongo
         $client = new Client('mongodb://127.0.0.1');
         
         // select database
-        self::$database = $client->getDatabase('test');
+        $this->database = $client->getDatabase('test');
+        $this->queue = $this->database->getQueue('queue_channel');
     }
     
-    public static function tearDownAfterClass() {
-
+    public function tearDown()
+    {
+        $this->queue->clear();
     }
     
     public function testQueueCount()
     {
-        $queue = self::$database->getQueue('queue_channel')->clear();
-        
-        $queue->enqueue(array(
+        $this->queue->enqueue(array(
             'param' => 1,
         ));
         
-        $queue->enqueue(array(
+        $this->queue->enqueue(array(
             'param' => 2,
         ));
         
-        $queue->enqueue(array(
+        $this->queue->enqueue(array(
             'param' => 3,
         ), 10);
         
         // check count of messages in queue
-        $this->assertEquals(3, count($queue));
+        $this->assertEquals(3, count($this->queue));
     }
     
     public function testDequeue()
     {
-        $queue = self::$database->getQueue('queue_channel')->clear();
-
         // add normal
-        $queue->enqueue(array(
+        $this->queue->enqueue(array(
             'param' => 1,
         ));
 
         // add with priority
-        $queue->enqueue('priority-driven', 10);
+        $this->queue->enqueue('priority-driven', 10);
 
         // add normal
-        $queue->enqueue(array(
+        $this->queue->enqueue(array(
             'param' => 3,
         ));
 
         // check if message with priority first
-        $this->assertEquals('priority-driven', $queue->dequeue());
+        $this->assertEquals('priority-driven', $this->queue->dequeue());
 
-        $this->assertEquals(1, $queue->dequeue()->get('param'));
+        $this->assertEquals(1, $this->queue->dequeue()->get('param'));
 
-        $this->assertEquals(3, $queue->dequeue()->get('param'));
+        $this->assertEquals(3, $this->queue->dequeue()->get('param'));
     }
     
     public function testDequeueFromNotExistedCollection()
     {
-        $queue = self::$database->getQueue('some_strange_unexisted_channel');
+        $queue = $this->database->getQueue('some_strange_unexisted_channel');
         
         $this->assertNull($queue->dequeue());
     }
