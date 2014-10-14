47eb60339ddb6cb8debf1560ba371747460f9f30
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-10-14 16:47:39 +0300

    add support of cancelling operation of evenmt handling cancelled

--- src/Event.php
+++ src/Event.php
@@ -8,6 +8,8 @@ class Event extends \Symfony\Component\EventDispatcher\Event
      * @var mixed $target target object, on which event is fired
      */
     private $_target;
+    
+    private $_cancelled = false;
 
     /**
      * Set target object, on which event is fired
@@ -27,5 +29,27 @@ class Event extends \Symfony\Component\EventDispatcher\Event
     public function getTarget()
     {
         return $this->_target;
+    }
+    
+    /**
+     * Check if operation execution cancelled
+     */
+    public function isCancelled()
+    {
+        return $this->_cancelled;
+    }
+    
+    /**
+     * Cancel related operation execution. If called as beforeInsert 
+     * handler, than insert will be cancelled.
+     */
+    public function cancel()
+    {
+        $this->_cancelled = true;
+        
+        // propagation already not need
+        $this->stopPropagation();
+        
+        return $this;
     }
 }
