c1bec6dd08bbe0869701eb255d2d3ca9919e4ff4
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-10-18 22:19:35 +0300

    add test

--- tests/DocumentVersioningTest.php
+++ tests/DocumentVersioningTest.php
@@ -18,6 +18,11 @@ class DocumentVersioningTest extends \PHPUnit_Framework_TestCase
         $this->collection = $database->getCollection('phpmongo_test_collection');
     }
     
+    public function tearDown()
+    {
+        $this->collection->delete();
+    }
+    
     public function testCreateRevisionOnUpdate()
     {
         // revision created only after update
@@ -35,5 +40,49 @@ class DocumentVersioningTest extends \PHPUnit_Framework_TestCase
         $revision = current($document->getRevisions());
         
         $this->assertEquals('value', $revision->get('param'));
+    }
+    
+    public function testGetRevisionKeys()
+    {
+        // revision created only after update
+        $document = $this->collection
+            ->createDocument(array('param' => 'value'))
+            ->save()
+            ->set('param', 'value1')
+            ->save()
+            ->set('param', 'value2')
+            ->save();
+        
+        $this->assertEquals(2, $document->getRevisionKeys());
+    }
+    
+    public function testGetRevision()
+    {
+        $document = $this->collection
+            ->createDocument(array('param' => 'value'))
+            ->save()
+            ->set('param', 'value1')
+            ->save()
+            ->set('param', 'value2')
+            ->save();
+        
+        $key = current($document->getRevisionKeys());
+        
+        $revision = $document->getRevision($key);
+        $this->assertEquals('value1', $revision->param);
+    }
+    
+    public function testGetRevisions()
+    {
+        $document = $this->collection
+            ->createDocument(array('param' => 'value'))
+            ->save()
+            ->set('param', 'value1')
+            ->save()
+            ->set('param', 'value2')
+            ->save();
+        
+        $revisions = $document->getRevisions(1, 1);
+        $this->assertEquals('value2', $revisions->param);
     }
 }
