021d57af3b8d44b757f7a940fcb14f3055c4b63e
Sokil <dmytro.sokil@gmail.com>
2016-11-21 13:53:35 +0200

    copyright

--- src/ArrayableInterface.php
+++ src/ArrayableInterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the PHPMongo package.
+ *
+ * (c) Dmytro Sokil <dmytro.sokil@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Sokil\Mongo;
 
