72ed2e01e0aaff029f413d6b17adcb364b87fadf
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-01-13 22:54:18 +0200

    run test from host mashine

--- docker/php/run-tests.sh
+++ docker/php/run-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 FILE=$(readlink -f $0);
-DIR=$(dirname $FILE)
+PROJECT_DIR=$(dirname $FILE)/../..
 
 PHP_VERSION=$(php -r "echo phpversion();");
-PHPUNIT_LOG_DIR=$DIR/../share/phpunit/${PHP_VERSION}
+PHPUNIT_LOG_DIR=$PROJECT_DIR/docker/share/phpunit/${PHP_VERSION}
 
 # prepare phpunit log dir
 if [[ ! -d $PHPUNIT_LOG_DIR ]];
@@ -15,9 +15,9 @@ else
 fi
 
 # start bunch of tests
-PHPMONGO_DSN=mongodb://mongodb24 ./vendor/bin/phpunit -c ./tests/phpunit.xml ./tests > ${PHPUNIT_LOG_DIR}/mongo24.log
-PHPMONGO_DSN=mongodb://mongodb26 ./vendor/bin/phpunit -c ./tests/phpunit.xml ./tests > ${PHPUNIT_LOG_DIR}/mongo26.log
-PHPMONGO_DSN=mongodb://mongodb30 ./vendor/bin/phpunit -c ./tests/phpunit.xml ./tests > ${PHPUNIT_LOG_DIR}/mongo30.log
-PHPMONGO_DSN=mongodb://mongodb32 ./vendor/bin/phpunit -c ./tests/phpunit.xml ./tests > ${PHPUNIT_LOG_DIR}/mongo32.log
-PHPMONGO_DSN=mongodb://mongodb33 ./vendor/bin/phpunit -c ./tests/phpunit.xml ./tests > ${PHPUNIT_LOG_DIR}/mongo33.log
-PHPMONGO_DSN=mongodb://mongodb34 ./vendor/bin/phpunit -c ./tests/phpunit.xml ./tests > ${PHPUNIT_LOG_DIR}/mongo34.log
+PHPMONGO_DSN=mongodb://mongodb24 $PROJECT_DIR/vendor/bin/phpunit -c $PROJECT_DIR/tests/phpunit.xml $PROJECT_DIR/tests > ${PHPUNIT_LOG_DIR}/mongo24.log
+PHPMONGO_DSN=mongodb://mongodb26 $PROJECT_DIR/vendor/bin/phpunit -c $PROJECT_DIR/tests/phpunit.xml $PROJECT_DIR/tests > ${PHPUNIT_LOG_DIR}/mongo26.log
+PHPMONGO_DSN=mongodb://mongodb30 $PROJECT_DIR/vendor/bin/phpunit -c $PROJECT_DIR/tests/phpunit.xml $PROJECT_DIR/tests > ${PHPUNIT_LOG_DIR}/mongo30.log
+PHPMONGO_DSN=mongodb://mongodb32 $PROJECT_DIR/vendor/bin/phpunit -c $PROJECT_DIR/tests/phpunit.xml $PROJECT_DIR/tests > ${PHPUNIT_LOG_DIR}/mongo32.log
+PHPMONGO_DSN=mongodb://mongodb33 $PROJECT_DIR/vendor/bin/phpunit -c $PROJECT_DIR/tests/phpunit.xml $PROJECT_DIR/tests > ${PHPUNIT_LOG_DIR}/mongo33.log
+PHPMONGO_DSN=mongodb://mongodb34 $PROJECT_DIR/vendor/bin/phpunit -c $PROJECT_DIR/tests/phpunit.xml $PROJECT_DIR/tests > ${PHPUNIT_LOG_DIR}/mongo34.log
--- run-docker-tests.sh
+++ run-docker-tests.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+echo "Test PHP 5.6"
+docker exec -it phpmongo_php56 bash /phpmongo/docker/php/run-tests.sh
+
+echo "Test PHP 7.0"
+docker exec -it phpmongo_php70 bash /phpmongo/docker/php/run-tests.sh
+
+echo "Test PHP 7.1"
+docker exec -it phpmongo_php71 bash /phpmongo/docker/php/run-tests.sh
