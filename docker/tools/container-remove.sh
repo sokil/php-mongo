72e11f4c5e3ef07f4525a8e29452fc1e52c046d7
Sokil <dmytro.sokil@gmail.com>
2017-02-20 11:39:57 +0200

    Update container-remove.sh

--- docker/tools/container-remove.sh
+++ docker/tools/container-remove.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
+# confirm
+while true; do
+    read -p "Do you really want to drop PHPMongo containers? (y/n): " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
 docker ps -a -f NAME=phpmongo --format "{{.Names}}" | xargs -I{} docker rm {}
