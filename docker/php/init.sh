6da8bc48a1edb408d1ca8213bc7442e8f9ac7a22
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-11-24 22:33:17 +0200

    fix docker env

--- docker/php/init.sh
+++ docker/php/init.sh
@@ -11,22 +11,23 @@ PROJECT_DIR="/phpmongo/"
 MONGO_EXT=$1
 if [[ -z $MONGO_EXT ]];
 then
-    MONGO_EXT="mongo"
+    echo "Mongo extension name not specified (use 'mongo' or 'mongodb')"
+    exit
 fi;
 
 # Mongo extension version
 MONGO_EXT_VERSION=$2
 if [[ -z $MONGO_EXT_VERSION ]];
 then
-    MONGO_EXT_VERSION="1.6.2"
+    echo "Mongo extension version not specified"
+    exit
 fi;
-
-# Host machine
-DOCKERHOST_IP="$(/sbin/ip route|awk '/default/ { print $3 }')";
-echo "$DOCKERHOST_IP dockerhost" >> /etc/hosts
 
 ### PHP version
 PHP_VERSION=$(php -r "echo phpversion();");
+
+# mongo version notification
+echo "Creating environment for MongoDB PHP extension '${MONGO_EXT}' ver. ${MONGO_EXT_VERSION} and PHP ${PHP_VERSION}"
 
 #####################################
 #        PHP extensions             #
@@ -36,18 +37,30 @@ if [[ -z $(dpkg -l | grep libssl-dev) ]];
 then
     # add library requirements
     apt-get update -q
-    apt-get install --no-install-recommends -y libssl-dev
+    apt-get install --no-install-recommends -y libssl-dev iproute2
 
     # install ext-zip
     apt-get install --no-install-recommends -y zlib1g-dev
     docker-php-ext-install zip
 
+    # update pecl
+    pecl channel-update pecl.php.net
+
     # install pecl mongo extension
     yes '' | pecl -q install -f ${MONGO_EXT}-${MONGO_EXT_VERSION}
     docker-php-ext-enable ${MONGO_EXT}.so
 
-    # XDEBUG
-    pecl install xdebug
+    # Docker host for XDEBUG
+    DOCKERHOST_IP="$(/sbin/ip route | awk '/default/ { print $3 }')";
+    echo "$DOCKERHOST_IP dockerhost" >> /etc/hosts
+
+    # last version of xdebug with support PHP < 7.0 is 2.5.5
+    if [[ ${PHP_VERSION:0:2} == "5." ]]; then
+        pecl install xdebug-2.5.5;
+    else
+        pecl install xdebug;
+    fi
+
     docker-php-ext-enable xdebug.so
 
     echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini
@@ -64,19 +77,20 @@ fi
 #        Composer                   #
 #####################################
 
-if [[  -z $(which composer) ]];
+if [[ -z $(which composer) ]];
 then
     # go to project dir
     cd $PROJECT_DIR
     # download composer
     curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer
     # add mongodb compatibility layer
-    if [[ $MONGO_EXT = "mongodb" ]];
+    if [[ $MONGO_EXT == "mongodb" ]];
     then
-        composer require "alcaeus/mongo-php-adapter" --ignore-platform-reqs
+        echo "Installing compatibility layer for new MongoDB extension"
+        composer require "alcaeus/mongo-php-adapter" --ignore-platform-reqs --no-interaction
+    else
+        composer install --no-interaction
     fi;
-    # update composer dependencies
-    composer update --no-interaction
 fi
 
 #####################################
